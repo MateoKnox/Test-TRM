f5db3ebc82cce1a69642360581e19ac632577814
MateoKnox <reiddobrien@gmail.com>
2026-02-13 13:43:11 +1300

    chore: snapshot 15/20 (2026-02-13)

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  {
+    ignores: [
+      '**/dist/**',
+      '**/artifacts/**',
+      '**/cache/**',
+      '**/node_modules/**',
+      '**/typechain-types/**',
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-console': 'off',
+    },
+  },
+];
